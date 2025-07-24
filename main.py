0ae96c7a9045e5aff8b232a4804e018fbc5ba5d0
Alibek <kanabekov.hero@gmail.com>
2025-07-24 18:07:25 +0500

    first commit

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class Task(BaseModel):
-    name:str
-    description: str | None
+@app.get("/")
+async def home() -> dict[str,str]:
+    return {"data":"message"}
 
-@app.get("/tasks")
-async def get_tasks():
-    task = Task(name="Запиши это видео")
-    return {"data": task}
+
+@app.get("/contacts")
+async def contacts() -> int:
+    return 34
+
+
+posts = [
+    {'id':1, 'title': 'New1', 'body': 'Text 1' },
+    {'id':2, 'title': 'New2', 'body': 'Text 2' },
+    {'id':3, 'title': 'New3', 'body': 'Text 3' },
+]
+
+
+@app.get("/items")
+async def get_items() -> list[dict]:
+    return posts
+
+
+@app.get("/items/{id}")
+async def get_items_by_id(id: int) -> list[dict]:
+    return posts
